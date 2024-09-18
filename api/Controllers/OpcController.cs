2c4249f606937797a11cd5297db48eefc860818c
andrei_potiomkin <89867134953@mail.ru>
2024-09-18 08:38:54 +0300

    feat(controller): (#3) implemented /browselocal endpoint

--- api/Controllers/OpcController.cs
+++ api/Controllers/OpcController.cs
@@ -1,11 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
-using OpcLabs.EasyOpc;
-using OpcLabs.EasyOpc.DataAccess;
-using OpcLabs.EasyOpc.OperationModel;
-using OpcLabs.EasyOpc.UA;
-using OpcLabs.EasyOpc.UA.Discovery;
-using OpcLabs.EasyOpc.UA.OperationModel;
 using api.Models.Opc;
+using api.Services.Impl;
+using OpcLabs.BaseLib.Extensions.Internal;
 
 namespace api.Controllers
 {
@@ -13,48 +9,21 @@ namespace api.Controllers
   [Route("[controller]")]
   public class OpcController: ControllerBase
   {
-    private static readonly EasyDAClient daClient = new EasyDAClient();
-    private static readonly EasyUAClient uaClient = new EasyUAClient();
+    private static readonly OpcService service = new();
 
-    [HttpGet("Da/BrowseLocal")]
-    public IActionResult browseLocalDa()
+    // GET /opc/browselocal
+    [HttpGet("/browselocal")]
+    public IActionResult BrowseLocal()
     {
       try
       {
-        var serverElementCollection = daClient.BrowseServers("");
-        var servers = serverElementCollection
-          .Select(server => new OpcDa(
-            server.Description,
-            server.ProgId,
-            server.UrlString))
-          .ToList();
+        OpcServer[] servers = service.BrowseServers(true);
 
         return Ok(servers);
       }
-      catch (OpcException daException)
+      catch (Exception ex)
       {
-        return BadRequest(new { error = daException.GetBaseException().Message });
-      }
-    }
-
-    [HttpGet("Ua/BrowseLocal")]
-    public IActionResult browseLocalUa()
-    {
-      try
-      {
-        var serverElementCollection = uaClient.DiscoverLocalServers("opcua.demo-this.com");
-        var servers = serverElementCollection
-          .Select(server => new OpcUa(
-            server.ApplicationName,
-            server.DiscoveryUriString,
-            server.ApplicationUriString))
-          .ToList();
-
-        return Ok(servers);
-      }
-      catch (OpcException daException)
-      {
-        return BadRequest(new { error = daException.GetBaseException().Message });
+        return BadRequest(ex.GetBaseMessage());
       }
     }
   }
