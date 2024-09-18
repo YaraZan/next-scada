b667c7bccbf77faf50b2d90845142c8213031d49
andrei_potiomkin <89867134953@mail.ru>
2024-09-18 08:42:34 +0300

    feat(controller): (#3) add OpcServer base class

---
+++ api/Models/Opc/OpcServer.cs
@@ -0,0 +1,11 @@
+namespace api.Models.Opc
+{
+    public abstract class OpcServer
+    {
+        public virtual string Url { get; set; }
+        public virtual string Name { get; set; }
+        public virtual string ConnectionString { get; set; }
+
+        public abstract string Type { get; }
+    }
+}
