bfcad474d07e067eb4ecd3a9d66458b0f2231759
andrei_potiomkin <89867134953@mail.ru>
2024-09-18 08:41:48 +0300

    hotfix(controller): (#3) change constructor to primary class in OpcDa

--- api/Models/Opc/OpcDa.cs
+++ api/Models/Opc/OpcDa.cs
@@ -1,22 +1,15 @@
 namespace api.Models.Opc
 {
-    public class OpcDa : Opc
+    public class OpcDa(
+        string description,
+        string progId,
+        string urlString
+        ) : OpcServer
     {
-        public OpcDa(
-            string description,
-            string progId,
-            string urlString
-        )
-        {
-            Name = description;
-            ConnectionString = progId;
-            Url = urlString;
-        }
+    public override string Url { get; set; } = urlString;
+    public override string Name { get; set; } = description;
+    public override string ConnectionString { get; set; } = progId;
 
-        public override string Url { get; set; }
-        public override string Name { get; set; }
-        public override string ConnectionString { get; set; }
-
-        public override string Type => "DA";
+    public override string Type => "DA";
     }
 }
