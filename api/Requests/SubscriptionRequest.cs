3fed7a5d09dd82369d024373ffd66eaadf1b43bb
andrei_potiomkin <89867134953@mail.ru>
2024-10-04 10:20:18 +0300

    hotfix(requests): (#9) change item path property to item id

--- api/Requests/SubscriptionRequest.cs
+++ api/Requests/SubscriptionRequest.cs
@@ -2,10 +2,10 @@ namespace api.Requests
 {
   public class SubscriptionRequest
   {
-      public string ConnectionString { get; set; }
-      public IEnumerable<string> ItemPaths { get; set; }
-      public bool IsDa { get; set; } = false;
-      public string? Host { get; set; }
+    public string ConnectionString { get; set; }
+    public string ItemId { get; set; }
+    public bool IsDa { get; set; } = false;
+    public string? Host { get; set; }
   }
 
 }
